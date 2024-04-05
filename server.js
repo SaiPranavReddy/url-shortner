50ce6e1c9e364ed488e7c69503ca5cb3ef11f2e8
Pranav Donthidi <pranavdonthidi@gmail.com>
2024-04-04 17:08:51 -0700

    added ShortUrl Logic

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const shortUrl = require('./models/shortUrl')
 const app = express()
 
+mongoose.connect('mongodb://localhost/urlshortener',{
+    useNewUrlParser:true, useUnifiedTopology: true
+})
 app.set('view engine','ejs')
-app.get('/',(req,res)=>{
-    res.render('index')
+app.use(express.urlencoded({extended: false}))
+app.get('/',async(req,res)=>{
+    const shortUrls = await shortUrl.find()
+    res.render('index',{shortUrls:shortUrls})
+})
+
+app.post('/shortUrls', async(req,res)=>{
+    await shortUrl.create({full:req.body.fullUrl})
+    res.redirect('/')
 })
 app.listen(process.env.PORT || 5001);
