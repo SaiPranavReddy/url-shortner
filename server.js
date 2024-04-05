1648c309d7f3d98566b987c7283d22f394a82edf
Pranav Donthidi <pranavdonthidi@gmail.com>
2024-04-04 17:42:37 -0700

    fixed the shortUrl Logic

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const shortUrl = require('./models/shortUrl')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrlModel = require('./models/shortUrl');
 
-mongoose.connect('mongodb://localhost/urlshortener',{
-    useNewUrlParser:true, useUnifiedTopology: true
-})
-app.set('view engine','ejs')
-app.use(express.urlencoded({extended: false}))
-app.get('/',async(req,res)=>{
-    const shortUrls = await shortUrl.find()
-    res.render('index',{shortUrls:shortUrls})
-})
+const app = express();
 
-app.post('/shortUrls', async(req,res)=>{
-    await shortUrl.create({full:req.body.fullUrl})
-    res.redirect('/')
-})
+mongoose.connect('mongodb://localhost/urlshortener');
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrlModel.find();
+  res.render('index', { shortUrls: shortUrls });
+});
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrlModel.create({ full: req.body.fullUrl });
+  res.redirect('/');
+});
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrlModel.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  await shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
 app.listen(process.env.PORT || 5001);
